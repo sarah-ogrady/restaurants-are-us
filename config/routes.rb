91fbbde5e60dc2f6ea6d256acbfbfdd6f27b36c3
Sarah OGrady <sarahfrancesogrady@gmail.com>
2021-11-09 10:00:27 +0000

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Read all restaurants (index)
+  get '/restaurants', to: 'restaurants#index'
+  # read one restaurant (show)
+  get '/restaurants/:id', to: 'restaurants#show', as: :restaurant
+  # create a restaurant (new + create)
+  get '/restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  post '/restaurants', to: 'restaurants#create'
+  # update a restaurant (edit + update)
+  get '/restaurants/:id/edit', to: 'restaurants#edit', as: :edit_restaurant
+  patch '/restaurants/:id', to: 'restaurants#update'
+  # delete a restaurant (destroy)
+  delete '/restaurants/:id', to: 'restaurants#destroy'
+  # resources :restaurants
 end
